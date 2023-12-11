eb24456dacbb867fdb684b0752b09df1fa3ced1c
Torsten Sandell <to6061sa-s@student.lu.se>
2023-12-11 12:21:07 +0100

    Solve eleventh day

---
+++ 11th/problem.py
@@ -0,0 +1,48 @@
+import re
+from math import sqrt, floor, ceil, gcd, inf
+from collections import Counter
+
+with open("./11th/problem_example_input.txt", mode='r', encoding='utf-8') as input:
+    lines = input.read().splitlines()
+
+SPACE = [list(line) for line in lines]
+
+rows_to_expand = []
+for i, line in enumerate(lines):
+    if re.match(r"^\.+$", line):
+        rows_to_expand += [i]
+
+cols_to_expand = []
+for col in range(len(SPACE[0])):
+    this_line = ""
+    for row in range(len(SPACE)):
+        this_line += SPACE[row][col]
+    if re.match(r"^\.+$", this_line):
+        cols_to_expand += [col]
+
+empty_row = ['.' for i in range(len(SPACE))]
+for row in reversed(rows_to_expand):
+    SPACE = SPACE[:row] + [empty_row] + SPACE[row:]
+
+for col in reversed(cols_to_expand):
+    for r, row in enumerate(SPACE.copy()):
+        SPACE[r] = row[:col] + ['.'] + row[col:]
+
+galaxy_coordinates = []
+for r, row in enumerate(SPACE):
+    for c, value in enumerate(row):
+        if value == '#':
+            galaxy_coordinates += [(r, c)]
+
+result = 0
+for i, coordinate in enumerate(galaxy_coordinates):
+    for other_coordinate in galaxy_coordinates[i:]:
+        diff = (abs(other_coordinate[0] - coordinate[0]), abs(other_coordinate[1] - coordinate[1]))
+        distance = diff[0] + diff[1]
+        result += distance
+
+print(result)
+
+
+#for line in SPACE:
+    #print(''.join(line))
---
+++ 11th/problem_2.py
@@ -0,0 +1,63 @@
+import re
+from math import sqrt, floor, ceil, gcd, inf
+from collections import Counter
+
+with open("./11th/problem_input.txt", mode='r', encoding='utf-8') as input:
+    lines = input.read().splitlines()
+
+SPACE = [list(line) for line in lines]
+
+empty_row = ['!' for i in range(len(SPACE[0]))]
+
+for i, line in enumerate(lines):
+    if re.match(r"^\.+$", line):
+        SPACE[i] = empty_row
+
+for col in range(len(SPACE[0])):
+    this_line = ""
+    for row in range(len(SPACE)):
+        this_line += SPACE[row][col]
+    if re.match(r"^[.!]+$", this_line):
+        for row in range(len(SPACE)):
+            SPACE[row][col] = '!'
+
+galaxy_coordinates = []
+for r, row in enumerate(SPACE):
+    for c, value in enumerate(row):
+        if value == '#':
+            galaxy_coordinates += [(r, c)]
+
+result = 0
+for i, coordinate in enumerate(galaxy_coordinates):
+    for other_coordinate in galaxy_coordinates[i:]:
+
+        diff = (abs(other_coordinate[0] - coordinate[0]), abs(other_coordinate[1] - coordinate[1]))
+        empty_spaces_crossed = 0
+
+        # Positive or negative direction
+        if diff[0] != 0:
+            iter_step_row = round((other_coordinate[0] - coordinate[0]) / diff[0])
+        else:
+            iter_step_row = 1
+
+        if diff[1] != 0:
+            iter_step_col = round((other_coordinate[1] - coordinate[1]) / diff[1])
+        else:
+            iter_step_col = 1            
+
+        for row in range(coordinate[0], other_coordinate[0], iter_step_row):
+            if SPACE[row][0] == '!':
+                empty_spaces_crossed += 1
+
+        for col in range(coordinate[1], other_coordinate[1], iter_step_col):
+            if SPACE[0][col] == '!':
+                empty_spaces_crossed += 1
+
+        distance = diff[0] + diff[1] + empty_spaces_crossed * (1000000 - 1)
+
+        #print(f"Total empty spaces crossed between {coordinate}, {other_coordinate}: {empty_spaces_crossed}")
+        #print(f"for a total distance of {distance}")
+
+        result += distance
+
+print(result)
